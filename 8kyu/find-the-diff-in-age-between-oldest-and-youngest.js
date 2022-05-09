42fadb271295dfe497c709b663d8d89cf15814c4
Victoria Cheng <victoria.cheng15@gmail.com>
2022-05-09 12:49:35 -0400

    add first solution

---
+++ 8kyu/find-the-diff-in-age-between-oldest-and-youngest.js
@@ -0,0 +1,20 @@
+/* 
+At the annual family gathering, the family likes to find the oldest living family member’s age and the youngest family member’s age and calculate the difference between them.
+
+You will be given an array of all the family members' ages, in any order. The ages will be given in whole numbers, so a baby of 5 months, will have an ascribed ‘age’ of 0. Return a new array (a tuple in Python) with [youngest age, oldest age, difference between the youngest and oldest age].
+*/
+
+/* 
+PREP:
+- input is array
+- find min age
+- find max age
+- calc the difference for oldest and youngest
+- return array [youngest, oldest, the difference]
+*/
+
+function differenceInAges(ages) {
+  const youngest = Math.min(...ages);
+  const oldest = Math.max(...ages);
+  return [youngest, oldest, oldest - youngest];
+}
---
+++ 8kyu/printing-array-elements-with0comma-delimiters.js
@@ -0,0 +1,20 @@
+/* 
+Input: Array of elements
+
+["h","o","l","a"]
+
+Output: String with comma delimited elements of the array in th same order.
+
+"h,o,l,a"
+*/
+
+/* 
+PREP:
+- input is array
+- get , between letters
+*/
+
+function printArray(array) {
+  // show me the code!
+  return array.join(',');
+}
