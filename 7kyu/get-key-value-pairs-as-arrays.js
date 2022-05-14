1ae51ce96d0e3eb95db28e5b11de8741bdd7be74
Victoria Cheng <victoria.cheng15@gmail.com>
2022-05-14 12:48:42 -0400

    add first solution

---
+++ 7kyu/get-key-value-pairs-as-arrays.js
@@ -0,0 +1,20 @@
+/* 
+Complete the keysAndValues function so that it takes in an object and returns the keys and values as separate arrays.
+
+Example:
+
+keysAndValues({a: 1, b: 2, c: 3}) // should return [['a', 'b', 'c'], [1, 2, 3]]
+Style Points (JS/CoffeeScript only): This kata only tests for data that uses object literal notation (simple objects). For extra style, can you get your method to check for objects that extend their prototype?
+*/
+
+/* 
+PREP:
+- input is obj
+- use Object.keys() to get property name
+- use Object.values() to get property value
+- output is an array with 2 subarrys
+*/
+
+function keysAndValues(data) {
+  return [Object.keys(data), Object.values(data)];
+}
---
+++ 8kyu/sum-of-differences-in-array.js
@@ -0,0 +1,26 @@
+/* 
+Your task is to sum the differences between consecutive pairs in the array in descending order.
+
+For example:
+
+sumOfDifferences([2, 1, 10])
+Returns 9
+
+Descending order: [10, 2, 1]
+
+Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
+
+If the array is empty or the array has only one element the result should be 0 (Nothing in Haskell).
+*/
+
+function sumOfDifferences(arr) {
+  const sorted = arr.sort((a, b) => b - a);
+  return sorted.reduce((acc, curr, index, array) => {
+    const next = array[index + 1];
+    const calc = curr - next;
+    if (!isNaN(calc)) {
+      acc += calc;
+    }
+    return acc;
+  }, 0);
+}
