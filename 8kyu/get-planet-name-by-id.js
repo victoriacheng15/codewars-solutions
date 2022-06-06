05be495fc5ec5e939f4cb10cda0058ba8bcd9fb2
Victoria Cheng <victoria.cheng15@gmail.com>
2022-06-06 11:49:55 -0400

    add first solution

---
+++ 8kyu/get-planet-name-by-id.js
@@ -0,0 +1,80 @@
+/* 
+The function is not returning the correct values. Can you figure out why?
+
+Example (Input --> Output ):
+
+3 --> "Earth"
+*/
+
+// function getPlanetName(id) {
+//   let name;
+//   switch (id) {
+//     case 1:
+//       name = 'Mercury';
+//     case 2:
+//       name = 'Venus';
+//     case 3:
+//       name = 'Earth';
+//     case 4:
+//       name = 'Mars';
+//     case 5:
+//       name = 'Jupiter';
+//     case 6:
+//       name = 'Saturn';
+//     case 7:
+//       name = 'Uranus';
+//     case 8:
+//       name = 'Neptune';
+//   }
+
+//   return name;
+// }
+
+function getPlanetName(id) {
+  let name;
+  switch (id) {
+    case 1:
+      name = 'Mercury';
+      break;
+    case 2:
+      name = 'Venus';
+      break;
+    case 3:
+      name = 'Earth';
+      break;
+    case 4:
+      name = 'Mars';
+      break;
+    case 5:
+      name = 'Jupiter';
+      break;
+    case 6:
+      name = 'Saturn';
+      break;
+    case 7:
+      name = 'Uranus';
+      break;
+    case 8:
+      name = 'Neptune';
+      break;
+  }
+
+  return name;
+}
+
+
+// or 
+
+function getPlanetName(id){
+  const getName = {
+    1: 'Mercury',
+    2: 'Venus',
+    3: 'Earth',
+    4: 'Mars',
+    5: 'Jupiter',
+    6: 'Saturn',
+    7: 'Uranus',
+    8: 'Neptune'
+  }
+  return getName[id]
+}
