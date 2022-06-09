ea1487a5c1d434b2c68402cb6058d31d05e6ad00
Victoria Cheng <victoria.cheng15@gmail.com>
2022-06-09 10:43:35 -0400

    add solutions with test

--- 8kyu/a-needle-in-the-haystack.test.js
+++ 8kyu/a-needle-in-the-haystack.test.js
@@ -20,3 +20,31 @@ function findNeedle(haystack) {
   const index = haystack.indexOf('needle');
   return `found the needle at position ${index}`;
 }
+
+describe('find needle', () => {
+  const haystack_1 = [
+    '3',
+    '123124234',
+    undefined,
+    'needle',
+    'world',
+    'hay',
+    2,
+    '3',
+    true,
+    false,
+  ];
+  const haystack_2 = [
+    '283497238987234',
+    'a dog',
+    'a cat',
+    'some random junk',
+    'a piece of hay',
+    'needle',
+    'something somebody lost a while ago',
+  ];
+  it('should return the index of needle', () => {
+    expect(findNeedle(haystack_1)).toMatch('found the needle at position 3');
+    expect(findNeedle(haystack_2)).toMatch('found the needle at position 5');
+  });
+});
