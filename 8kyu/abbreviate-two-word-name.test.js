e51440c5e67dd784ca8c222ff6747f3297b99a6b
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2022-06-13 14:31:33 +0000

    update solution with jest

--- 8kyu/abbreviate-two-word-name.test.js
+++ 8kyu/abbreviate-two-word-name.test.js
@@ -22,3 +22,10 @@ function abbrevName(name) {
   const upperCase = (word) => word[0][0].toUpperCase();
   return name.split(' ').map(upperCase).join('.');
 }
+
+describe('abbrev name', () => {
+  it('should return abbrev name, e.g. P.F', () => {
+    expect(abbrevName('Sam Harris')).toEqual('S.H');
+    expect(abbrevName('patrick feeney')).toEqual('P.F');
+  });
+});
---
+++ 8kyu/the-feast-of-many-beasts.test.js
@@ -0,0 +1,35 @@
+/* 
+All of the animals are having a feast! Each animal is bringing one dish. There is just one rule: the dish must start and end with the same letters as the animal's name. For example, the great blue heron is bringing garlic naan and the chickadee is bringing chocolate cake.
+
+Write a function feast that takes the animal's name and dish as arguments and returns true or false to indicate whether the beast is allowed to bring the dish to the feast.
+
+Assume that beast and dish are always lowercase strings, and that each has at least two letters. beast and dish may contain hyphens and spaces, but these will not appear at the beginning or end of the string. They will not contain numerals.
+*/
+
+// function feast(beast, dish) {
+//   return beast[0] === dish[0] && beast.at(-1) === dish.at(-1);
+// }
+
+/* 
+can use slice(-1) instead
+
+e.g. beast.slice(-1)
+
+beast.slice(-1) === dish.slice(-1)
+*/
+
+function feast(beast, dish) {
+  return (
+    beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length - 1]
+  );
+}
+
+describe('feast', () => {
+  it('should return true if first and last letters are the same', () => {
+    expect(feast('great blue heron', 'garlic naan')).toBeTruthy();
+  });
+
+  it('should return false if first and last letters are NOT the same', () => {
+    expect(feast('brown bear', 'bear claw')).toBeFalsy();
+  });
+});
