6fa421d07434adf6999c0c46f5b457e3e9c56cd1
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2022-06-14 13:11:49 +0000

    update solution with jest

--- 8kyu/add-length.test.js
+++ 8kyu/add-length.test.js
@@ -21,3 +21,10 @@ function addLength(str) {
   const array = str.split(' ');
   return array.map((word) => `${word} ${word.length}`);
 }
+
+describe('add length', () => {
+  it('should return an array of string with bane and length', () => {
+    expect(addLength('apple ban')).toEqual(['apple 5', 'ban 3']);
+    expect(addLength('you will win')).toEqual(['you 3', 'will 4', 'win 3']);
+  });
+});
